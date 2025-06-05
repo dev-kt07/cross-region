b526c2809c80aa6133a8e1202e55d07dc6946752
dev-kt07 <krishnatonk939@gmail.com>
2025-06-05 12:58:28 +0530

    aded db name

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ DB_CONFIG = {
     'host': 'my-auora-db.cluster-cfoeioosoz1v.ap-south-1.rds.amazonaws.com',
     'user': 'admin',
     'password': 'krishna123456',
-    'database': 'my-aurora-db'
+    'database': 'my-auora-db'
 }
